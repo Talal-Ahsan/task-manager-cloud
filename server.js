3b59351a2fd3a827e1ab466671c8b934bf04d358
Talal Ahsan <talalahsan6824@gmail.com>
2026-03-13 21:59:45 +0500

    Initial commit - Task Manager Cloud

---
+++ server.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const mysql = require("mysql2");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+/* -------------------- VIEW ENGINE -------------------- */
+
+app.set("view engine", "ejs");
+
+/* -------------------- MIDDLEWARE -------------------- */
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+/* -------------------- DATABASE CONNECTION -------------------- */
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "taskdb"
+});
+
+db.connect((err) => {
+    if (err) {
+        console.log("Database connection failed");
+        throw err;
+    }
+    console.log("Database Connected");
+});
+
+/* -------------------- ROUTES -------------------- */
+
+// Home Page
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+// Show all tasks
+app.get("/tasks", (req, res) => {
+    db.query("SELECT * FROM tasks", (err, results) => {
+        if (err) throw err;
+        res.render("tasks", { tasks: results });
+    });
+});
+
+// Add task
+app.post("/add-task", (req, res) => {
+    const task = req.body.task;
+
+    db.query(
+        "INSERT INTO tasks (task) VALUES (?)",
+        [task],
+        (err) => {
+            if (err) throw err;
+            res.redirect("/tasks");
+        }
+    );
+});
+
+// Delete task
+app.get("/delete/:id", (req, res) => {
+    const id = req.params.id;
+
+    db.query(
+        "DELETE FROM tasks WHERE id = ?",
+        [id],
+        (err) => {
+            if (err) throw err;
+            res.redirect("/tasks");
+        }
+    );
+});
+
+/* -------------------- SERVER -------------------- */
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
